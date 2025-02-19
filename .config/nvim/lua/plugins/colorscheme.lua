1be933898f7d2c342f96ca6884bf2eb66a11bed5
DCIMEE <backslash74@outlook.com>
2025-02-19 20:13:19 +1300

    nvim_colorscheme_upd

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,10 +1,12 @@
 return {
 	{
-		'ishan9299/nvim-solarized-lua',
+		'ellisonleao/gruvbox.nvim', 
+		priority = 1000, 
+		config = true,
 		lazy = false,
 		config = function()
 			-- load the color scheme here
-			vim.cmd([[colorscheme solarized-high]])
+			vim.cmd([[colorscheme gruvbox]])
 		end,
 	},
 }
---
+++ .config/nvim/lua/plugins/colorscheme.lua.solarized
@@ -0,0 +1,10 @@
+return {
+	{
+		'ishan9299/nvim-solarized-lua',
+		lazy = false,
+		config = function()
+			-- load the color scheme here
+			vim.cmd([[colorscheme solarized-high]])
+		end,
+	},
+}
